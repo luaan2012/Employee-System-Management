d02b876f53f5ca470e48dc878e53375b25d85c80
luaan2012 <oempreg.yes@outlook.com.br>
2024-01-27 21:54:11 -0300

    save

--- BaseLibrary/Entities/ApplicationUser.cs
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -2,6 +2,9 @@
 {
     public class ApplicationUser
     {
-
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? FullName { get; set; }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
+using ServerLibrary.Repositories.Contracts;
+using ServerLibrary.Repositories.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
